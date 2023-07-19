6a59f7bd9ba8f7c4d821fed425ae72e23e25474d
Estêfenis <lopefl89@gmail.com>
2023-07-19 11:11:34 -0300

    firts commit

---
+++ WebAppAPINoHttps/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppAPINoHttps.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //ou
+    //===> [Route("WeatherForecast")] <====
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        //ou
+        //===> [HttpGet()]
+        //[Route("GetWeatherForecast")] <====
+        
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
