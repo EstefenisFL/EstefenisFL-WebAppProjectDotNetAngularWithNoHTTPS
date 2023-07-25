3fd5cf99461260cf1d556e843c4f9e6d9d855fc3
Estêfenis <lopefl89@gmail.com>
2023-07-25 10:55:23 -0300

    add secutiry for secrets like connection strings using json writen in appsettings.json

--- ProjectWebData/ApplicationContext.cs
+++ ProjectWebData/ApplicationContext.cs
@@ -1,5 +1,8 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +14,26 @@ namespace ProjectWebData
     public class ApplicationContext : DbContext
     {
         public DbSet<Order> Pedidos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
             base.OnConfiguring(optionsBuilder);
+            //<===== Quando a aplicação estiver em um servidor ====>
             //optionBuilder.UseSqlServer("Server = DESKTOP-3E8SV0G\\SQLEXPRESS; Database = WebApplicationDataBaseForStudy; Trusted_Connection = True;");
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3E8SV0G\\SQLEXPRESS; Initial Catalog=WebApplicationDataBaseForStudy; Trusted_Connection = True;");
+
+            //<===== Quando a aplicação rodar local ====>
+            //setar string hard coded
+            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-3E8SV0G\\SQLEXPRESS; Initial Catalog=WebApplicationDataBaseForStudy; Trusted_Connection = True;");
+            //ou
+            //prover connection string do arquivo appsettings.json(obs:instalar pacotes necessários)
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("SQLServerDatabase"));
+
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ClientConfiguration());
